04075ce53b6894e2c5c292a90805c1fc243d1f14
gaito yamada <gaito021229@au.com>
2022-04-13 14:49:12 +0900

    if分の追加

--- Project2/ソース.cpp
+++ Project2/ソース.cpp
@@ -25,10 +25,31 @@ int main()
 	cout << "あなたのBMIは" << fixed << setprecision(1) << bmi << "になります。" << endl;
 	cout << "あなたの標準体重は" << stdWeight << "kgになります。" << endl;
 
-	if (bmi >= 25.0) 
+	if(bmi<18.5)
 	{
-		cout<<"あなたは"
+		cout << "あなたは低体重です。";
 	}
+	else if (18.5<=bmi< 25.0) 
+	{
+		cout << "あなたは標準体重です。";
+	}
+	else if (25<=bmi<30) 
+	{
+		cout<<"あなたは肥満(1)です。";
+	}
+	else if (30<=bmi<35)
+	{
+		cout << "あなたは肥満(2)です。";
+	}
+	else if (35 <= bmi < 40)
+	{
+		cout << "あなたは肥満(3)です。";
+	}
+	else if (40 <= bmi < 45)
+	{
+		cout << "あなたは肥満(4)です。";
+	}
+
 
 	return 0;
 }
